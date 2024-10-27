ec3e8b6f8cd9b48e7e9c6e7a8da11429991bbd9c
Mehul Bansal <mehulbansal83606@gmail.com>
2024-10-27 19:23:18 +0530

    routinng and order of route extension

--- src/app.js
+++ src/app.js
@@ -4,6 +4,23 @@ const app=express();
 
 const port =7777;
 
+// app.use("/",(req,res)=>{
+//     res.send("home");
+// })
+
+
+app.use("/hello",(req,res)=>{
+    res.send("hello");
+})
+
+app.use("/test",(req,res)=>{
+    res.send("test");
+})
+
+
+app.use("/hello/ancd",(req,res)=>{
+    res.send("fgjklrd");
+})
 
 app.listen(port,()=>{
     console.log(`server listening on port ${port}`);
