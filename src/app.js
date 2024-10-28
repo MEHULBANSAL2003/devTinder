c4dd1b3d71e7cc18224269f8b600b6edaf139824
Mehul Bansal <mehulbansal83606@gmail.com>
2024-10-28 18:07:06 +0530

    added validatiosn to schema"

--- src/app.js
+++ src/app.js
@@ -1,73 +1,61 @@
-const express=require("express");
+const express = require("express");
 const { connectDB } = require("./config/database.js");
-const User=require("./models/user.js")
-const app=express();    
-const port =7777;
+const User = require("./models/user.js");
+const app = express();
+const port = 7777;
 
 app.use(express.json()); // middleware to parse the data to json from client;
 
-app.post("/signup",async (req,res)=>{
+app.post("/signup", async (req, res) => {
+  const obj = req.body;
+  console.log(obj);
 
-
-     //console.log(req.body);
-     const obj=req.body;
-     console.log(obj);
-    const user=new User(obj);
-
-    // await user.save();
-    // const userObj={
-    //     firstName:"Mehul",
-    //     lastName:"Bansal",
-    //     emailId:"mehul@bansal.com",
-    //     password:"Mehul@123"
-    // }
-
-    // const user=new User(userObj);
-
-    try{
+  try {
+    const user = new User(obj);
     await user.save();
+    console.log(user);
 
     res.send("user added successfully");
-    }
-    catch(err){
-        res.status(400).send("an error occured ", err);
-    }
-
-})
-
-
-app.get("/feed",async (req,res)=>{
-  
-  try{
-  const data=await User.find();
-  if(data.length===0){
-    res.status(404).send("no user found");
-  }else{
-  res.send(data);
-  }  
-}
-  catch(err){
-    console.log("something went wrong", err);
+  } catch (err) {
+    res.status(400).send("an error occured " + err);
   }
-  
-})
+});
 
+app.get("/feed", async (req, res) => {
+  try {
+    const data = await User.find();
+    if (data.length === 0) {
+      res.status(404).send("no user found");
+    } else {
+      res.send(data);
+    }
+  } catch (err) {
+    console.log("something went wrong" + err);
+  }
+});
 
-
-
-
-
-
-
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    }); //runvalidators is to apply schema validations on updating thje existing document
+    console.log(user);
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("an error occured " + err);
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("connected to db succesfuly");
-    app.listen(port,()=>{
-        console.log(`server listening on port ${port}`);
-    })
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
   })
   .catch((err) => {
     console.log("an error occured while connecting to database", err);
   });
-
--- src/models/user.js
+++ src/models/user.js
@@ -6,27 +6,58 @@ const {Schema}=mongoose;
 const userSchema= new Schema({
 
     firstName:{
-        type: String
+        type: String,
+        required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName:{
-        type:String
+        type:String,
+
     },
     emailId:{
-        type:String
+        type:String,
+        lowercase:true,
+        required:true,
+        unique:true,
+        trim:true,
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        
     },
     gender:{
-          type:String
-    }
+          type:String,
+          // validate method will be called only when the new document is created
+          // on updating existing document it does not check for the validations
+          validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+          }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://img.freepik.com/premium-vector/businessman-avatar-illustration-cartoon-user-portrait-user-profile-icon_118339-4382.jpg",
+    },
+    about:{
+        type:String,
+        default: "This is the default description of the user"
+    },
+    skills:{
+        type:[String],
+    },
 
 
 
-});
+
+
+},{timestamps:true});
 
 
 const User=mongoose.model("User",userSchema);
