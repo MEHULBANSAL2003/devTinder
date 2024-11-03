05a21da469fa183fea0dc17f0716c78be3991017
Mehul Bansal <mehulbansal83606@gmail.com>
2024-11-04 00:04:21 +0530

    using ref and populate to link collections

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,41 +1,31 @@
-const express=require("express");
-const userRouter=express.Router();
-const ConnectionRequestModel=require("../models/connectionRequests");
+const express = require("express");
+const userRouter = express.Router();
+const ConnectionRequestModel = require("../models/connectionRequests");
 
-const {userAuth}=require("../middlewares/auth");
+const { userAuth } = require("../middlewares/auth");
 
 // get all the pending connection requests for the logged in user
-userRouter.get("/user/requests/recieved",userAuth, async (req,res)=>{
+userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
+  try {
+    const currUser = req.user;
 
-    try{
-
-    const currUser=req.user;
-
-    const requests=await ConnectionRequestModel.find({toUserId:currUser._id,status:"interested"});
+    const requests = await ConnectionRequestModel.find({
+      toUserId: currUser._id,
+      status: "interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
 
     console.log(requests);
 
     res.json({
-        result:"success",
-        message:requests
-    })
-}
-catch (err) {
+      result: "success",
+      message: requests,
+    });
+  } catch (err) {
     res.status(400).json({
       result: "error",
       message: `ERROR : ${err.message}`,
     });
   }
+});
 
-
-
-
-
-
-
-})
-
-
-
-
-module.exports={userRouter};
+module.exports = { userRouter };
