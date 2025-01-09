1bbaf6ac0c52a843a59162639f874673ec0a8ccf
Mehul Bansal <mehulbansal83606@gmail.com>
2025-01-09 22:09:35 +0530

    setup basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import Navbar from "./components/Navbar"
+import Body from "./components/Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
-
-  <Navbar/>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<div> login page</div>} />
+            <Route path="/signup" element={<div>signup page</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
